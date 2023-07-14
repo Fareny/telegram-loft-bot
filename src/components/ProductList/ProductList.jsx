80867d035b99db591cd4eb9a4b3f3a204b4a8854
unknown <pd.fareny@gmail.com>
2023-07-14 19:39:29 +0300

    test

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import './ProductList.css';
-import ProductItem from "../ProductItem/ProductItem";
-import { useTelegram } from "../../hooks/useTelegram";
-import { useCallback, useEffect } from "react";
+import ProductItem from '../ProductItem/ProductItem';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const products = [
     { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
@@ -13,77 +12,77 @@ const products = [
     { id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая' },
     { id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые' },
     { id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая' },
-]
+];
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
+        return acc += item.price;
+    }, 0);
+};
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const { tg, queryId } = useTelegram();
 
-    const onSendData = useCallback(() => {
-
+    const onSendData = () => {
         const data = {
             products: 'Джинсы',
             totalPrice: 5000,
-            queryId,
-        }
+            queryId: 434414124,
+        };
         console.log(data);
-        fetch('https://81.200.148.109:8000/web-data', {
+        fetch('http://81.200.148.109:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
-        })
-
-    }, [addedItems])
+        });
+    };
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData);
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
+    }, []);
 
     const onAdd = (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        const alreadyAdded = addedItems.find((item) => item.id === product.id);
         let newItems = [];
 
         if (alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
+            newItems = addedItems.filter((item) => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems)
+        setAddedItems(newItems);
 
         if (newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
-            })
+                text: `Купить ${getTotalPrice(newItems)}`,
+            });
         }
-    }
+    };
 
     return (
         <>
-            <div className={'list'}>
-                {products.map(item => (
+            <div className="list">
+                {products.map((item) => (
                     <ProductItem
                         product={item}
                         onAdd={onAdd}
-                        className={'item'}
+                        className="item"
                     />
                 ))}
             </div>
-            <button type='button' onClick={onSendData}>сука работай</button>
+            <button type="button" onClick={onSendData}>
+                сука работай
+            </button>
         </>
     );
 };
