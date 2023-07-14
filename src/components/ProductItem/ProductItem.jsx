b07869b28a6442051136259f1c17c0e0545b4c41
unknown <pd.fareny@gmail.com>
2023-07-14 14:28:08 +0300

    test

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../Button/Button'
+import './productitem.css'
 
 function ProductItem({ product, className, onAdd }) {
 
@@ -10,7 +11,7 @@ function ProductItem({ product, className, onAdd }) {
     return (
         <div className={'product ' + className}>
 
-            <div className={'img'} />
+            <img src='https://phonoteka.org/uploads/posts/2023-03/1679622942_phonoteka-org-p-kokteili-oboi-oboi-instagram-50.jpg' alt='' />
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
